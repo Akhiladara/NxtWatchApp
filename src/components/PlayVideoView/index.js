5f5bd11c84992b69a1d3dbe481ba4d457607d392
Akhiladara <daraakhila2002@gmail.com>
2023-12-04 10:42:15 +0000

    solution

---
+++ src/components/PlayVideoView/index.js
@@ -0,0 +1,126 @@
+import ReactPlayer from 'react-player'
+import {AiOutlineLike, AiOutlineDislike} from 'react-icons/ai'
+import {BiListPlus} from 'react-icons/bi'
+
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+
+import {
+  VideoPlayer,
+  PlayVideoTitle,
+  PLayVideoStatus,
+  PlayVideoStatusContainer,
+  PLayVideoDot,
+  PlaySocialButtonsContainer,
+  SocialButton,
+  ButtonText,
+  HrLine,
+  ChannelImage,
+  ChannelContainer,
+  ChannelInfo,
+  ChannelName,
+  ChannelSubscribers,
+  ChannelDescription,
+  ButtonContainer,
+} from './StyledComponents'
+
+const PlayVideoView = props => {
+  const {videoDetails, isLiked, isDisLiked, clickLiked, clickDisLiked} = props
+
+  const onClickLike = () => {
+    clickLiked()
+  }
+
+  const onClickDislike = () => {
+    clickDisLiked()
+  }
+
+  return (
+    <ThemeAndVideoContext.Consumer>
+      {value => {
+        const {isDarkTheme, addVideo, savedVideos} = value
+        const textColor = isDarkTheme ? '#64748b' : '#231f20'
+        let isSaved
+        const index = savedVideos.findIndex(
+          eachVideo => eachVideo.id === videoDetails.id,
+        )
+        if (index === -1) {
+          isSaved = false
+        } else {
+          isSaved = true
+        }
+
+        const saveIconColor = isSaved ? '#2563eb' : textColor
+
+        const onClickSave = () => {
+          addVideo(videoDetails)
+        }
+
+        return (
+          <VideoPlayer>
+            <ReactPlayer url={videoDetails.videoUrl} controls width="100%" />
+            <PlayVideoTitle color={textColor}>
+              {videoDetails.title}
+            </PlayVideoTitle>
+            <PlayVideoStatusContainer>
+              <PLayVideoStatus color={textColor}>
+                {videoDetails.viewCount} views
+                <PLayVideoDot>&#8226;</PLayVideoDot>
+                {videoDetails.publishedAt}
+              </PLayVideoStatus>
+              <PlaySocialButtonsContainer>
+                <ButtonContainer>
+                  <SocialButton
+                    type="button"
+                    color={isLiked ? '#2563eb' : '#64748b'}
+                    onClick={onClickLike}
+                  >
+                    <AiOutlineLike size={25} />
+                    <ButtonText>Like</ButtonText>
+                  </SocialButton>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <SocialButton
+                    type="button"
+                    color={isDisLiked ? '#2563eb' : '#64748b'}
+                    onClick={onClickDislike}
+                  >
+                    <AiOutlineDislike size={25} />
+                    <ButtonText>Dislike</ButtonText>
+                  </SocialButton>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <SocialButton
+                    type="button"
+                    color={saveIconColor}
+                    onClick={onClickSave}
+                  >
+                    <BiListPlus size={25} />
+                    <ButtonText>{isSaved ? 'Saved' : 'Save'}</ButtonText>
+                  </SocialButton>
+                </ButtonContainer>
+              </PlaySocialButtonsContainer>
+            </PlayVideoStatusContainer>
+            <HrLine />
+            <ChannelContainer>
+              <ChannelImage
+                src={videoDetails.profileImageUrl}
+                alt="channel logo"
+              />
+              <ChannelInfo>
+                <ChannelName color={textColor}>{videoDetails.name}</ChannelName>
+                <ChannelSubscribers color={textColor}>
+                  {videoDetails.subscriberCount} Subscribers
+                </ChannelSubscribers>
+                <ChannelDescription color={textColor}>
+                  {videoDetails.description}
+                </ChannelDescription>
+              </ChannelInfo>
+            </ChannelContainer>
+          </VideoPlayer>
+        )
+      }}
+    </ThemeAndVideoContext.Consumer>
+  )
+}
+
+export default PlayVideoView
